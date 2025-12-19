995e6a1d00a9cdc64915577a7e29107c72cb71cd
Anand Mohan <andmhn@outlook.com>
2025-12-19 16:33:25 +0530

    extracting away large chunks of ui code and callbacks

--- src/main.rs
+++ src/main.rs
@@ -41,17 +41,11 @@ fn load_css() {
                 }\n";
     let provider = gtk::CssProvider::new();
     provider.load_from_data(css);
-    let display = gtk::gdk::Display::default();
-    match display {
-        Some(display) => {
-            gtk::style_context_add_provider_for_display(
-                &display,
-                &provider,
-                gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
-            );
-        }
-        None => {}
-    }
+    gtk::style_context_add_provider_for_display(
+        &gtk::gdk::Display::default().expect("Could not connect to a display."),
+        &provider,
+        gtk::STYLE_PROVIDER_PRIORITY_APPLICATION,
+    );
 }
 
 fn create_editor_ui(window: &gtk::ApplicationWindow, sm: StateManager) {
@@ -66,43 +60,7 @@ fn create_editor_ui(window: &gtk::ApplicationWindow, sm: StateManager) {
     window.set_child(Some(&grid));
 
     // TOP BAR ------------------------------------------------------
-    let top_bar = gtk::Box::new(gtk4::Orientation::Horizontal, 5);
-    let file_entry = gtk::Entry::builder()
-        .placeholder_text("Enter the file path here...")
-        .hexpand(true)
-        .build();
-    let sm_clone = sm.clone();
-    let file_entry_buffer = file_entry.buffer();
-    file_entry.connect_changed(move |_| {
-        sm_clone.set_path(file_entry_buffer.text().as_str());
-    });
-    top_bar.append(&file_entry);
-
-    let sm_clone = sm.clone();
-    let picker_btn = gtk::Button::with_label("Select File");
-    let wn_clone = window.clone();
-    let fb = file_entry.buffer();
-    picker_btn.connect_clicked(move |_| {
-        let file_dialog = gtk::FileDialog::new();
-        let fb_clone = fb.clone();
-        let sm_clone = sm_clone.clone();
-        file_dialog.open(
-            Some(&wn_clone),
-            gtk::gio::Cancellable::NONE,
-            move |result| match result {
-                Ok(file) => {
-                    let path = file.path().expect("Failed to get path");
-                    fb_clone.set_text(path.display().to_string());
-                    sm_clone.set_path(path.display().to_string().as_str());
-                    sm_clone.log(format!("Selected file: {:?}", path));
-                }
-                Err(err) => {
-                    sm_clone.log(format!("Error selecting file: {}", err));
-                }
-            },
-        );
-    });
-    top_bar.append(&picker_btn);
+    let top_bar = create_topbar(window, &sm);
 
     // TEXT AREA ----------------------------------------------------
     let spacer = gtk::Box::new(gtk4::Orientation::Horizontal, 0);
@@ -120,6 +78,39 @@ fn create_editor_ui(window: &gtk::ApplicationWindow, sm: StateManager) {
     scrolled_text_area.add_css_class("scrolled_text_area");
 
     // ACTION BAR ---------------------------------------------------
+    let action_bar = create_action_bar(&sm, text_area);
+
+    grid.attach(&top_bar, 0, 0, 1, 1);
+    grid.attach(&spacer, 0, 1, 1, 1);
+    grid.attach(&description, 0, 2, 1, 1);
+    grid.attach(&scrolled_text_area, 0, 3, 1, 1);
+    grid.attach(&action_bar, 0, 4, 1, 1);
+    grid.attach(&sm.state.borrow().status_area, 0, 5, 1, 1);
+}
+
+fn create_topbar(window: &gtk4::ApplicationWindow, sm: &StateManager) -> gtk4::Box {
+    let top_bar = gtk::Box::new(gtk4::Orientation::Horizontal, 5);
+    let file_entry = gtk::Entry::builder()
+        .placeholder_text("Enter the file path here...")
+        .hexpand(true)
+        .build();
+    let sm_clone = sm.clone();
+    let file_entry_buffer = file_entry.buffer();
+    file_entry.connect_changed(move |_| {
+        sm_clone.set_path(file_entry_buffer.text().as_str());
+    });
+    top_bar.append(&file_entry);
+
+    let picker_btn = gtk::Button::with_label("Select File");
+    let sm_clone = sm.clone();
+    let wn_clone = window.clone();
+    let fb = file_entry.buffer();
+    picker_btn.connect_clicked(move |_| handle_file_pick(&sm_clone, &wn_clone, &fb));
+    top_bar.append(&picker_btn);
+    top_bar
+}
+
+fn create_action_bar(sm: &StateManager, text_area: gtk4::TextView) -> gtk4::Box {
     let action_bar = gtk::Box::new(gtk::Orientation::Horizontal, 5);
     let n_label = gtk::Label::builder().name("times ").build();
 
@@ -137,58 +128,81 @@ fn create_editor_ui(window: &gtk::ApplicationWindow, sm: StateManager) {
     let repeat_btn = gtk::Button::builder().label("Repeat Text").build();
     let bfr = text_area.buffer().clone();
     let sm_clone = sm.clone();
-    repeat_btn.connect_clicked(move |_| {
-        let str_prev = bfr.text(&bfr.start_iter(), &bfr.end_iter(), true);
-        if str_prev.len() == 0 {
-            sm_clone.log("Skipping : Text area is empty");
-            return;
-        }
-        let n = sm_clone.get_times();
-        for _ in 1..n {
-            bfr.insert(&mut bfr.end_iter(), str_prev.as_str());
-        }
-        sm_clone.log(format!("repeated {} times", n));
-    });
+    repeat_btn.connect_clicked(move |_| handle_repeat(&sm_clone, &bfr));
 
-    let spacer2 = gtk::Box::builder().hexpand(true).build();
+    let spacer = gtk::Box::builder().hexpand(true).build();
     let append_btn = gtk::Button::builder().label("Append Text to File").build();
     let sm_clone = sm.clone();
     let bfr = text_area.buffer().clone();
-    append_btn.connect_clicked(move |_| {
-        let path = sm_clone.get_path();
-        let text = bfr.text(&bfr.start_iter(), &bfr.end_iter(), true);
-
-        let file = std::fs::OpenOptions::new()
-            .append(true)
-            .create(false)
-            .open(&path);
-
-        match file {
-            Ok(mut file) => match file.write_all(text.as_bytes()) {
-                Err(why) => sm_clone.log(format!("couldn't append to {}: {}", path, why)),
-                Ok(_) => sm_clone.log(format!("successfully appended to {}", path)),
-            },
-            Err(why) => sm_clone.log(format!("Couldn't Open File: {} : {}", path, why)),
-        }
-    });
+    append_btn.connect_clicked(move |_| handle_append(&sm_clone, &bfr));
 
     action_bar.append(&n_label);
     action_bar.append(&times_input);
     action_bar.append(&repeat_btn);
-    action_bar.append(&spacer2);
+    action_bar.append(&spacer);
     action_bar.append(&append_btn);
-
-    grid.attach(&top_bar, 0, 0, 1, 1);
-    grid.attach(&spacer, 0, 1, 1, 1);
-    grid.attach(&description, 0, 2, 1, 1);
-    grid.attach(&scrolled_text_area, 0, 3, 1, 1);
-    grid.attach(&action_bar, 0, 4, 1, 1);
-    grid.attach(&sm.state.borrow().status_area, 0, 5, 1, 1);
+    action_bar
 }
 
-//==========================================================
+// ==================================================================
+// EVENTS
+// ==================================================================
+
+fn handle_file_pick(sm: &StateManager, wn: &gtk4::ApplicationWindow, fb: &gtk4::EntryBuffer) {
+    let file_dialog = gtk::FileDialog::new();
+    let fb = fb.clone();
+    let sm = sm.clone();
+    file_dialog.open(
+        Some(wn),
+        gtk::gio::Cancellable::NONE,
+        move |result| match result {
+            Ok(file) => {
+                let path = file.path().expect("Failed to get path");
+                fb.set_text(path.display().to_string());
+                sm.set_path(path.display().to_string().as_str());
+                sm.log(format!("Selected file: {:?}", path));
+            }
+            Err(err) => {
+                sm.log(format!("Error selecting file: {}", err));
+            }
+        },
+    );
+}
+
+fn handle_repeat(sm: &StateManager, bfr: &gtk4::TextBuffer) {
+    let str_prev = bfr.text(&bfr.start_iter(), &bfr.end_iter(), true);
+    if str_prev.len() == 0 {
+        sm.log("Skipping : Text area is empty");
+        return;
+    }
+    let n = sm.get_times();
+    for _ in 1..n {
+        bfr.insert(&mut bfr.end_iter(), str_prev.as_str());
+    }
+    sm.log(format!("repeated {} times", n));
+}
+
+fn handle_append(sm: &StateManager, bfr: &gtk4::TextBuffer) {
+    let path = sm.get_path();
+    let text = bfr.text(&bfr.start_iter(), &bfr.end_iter(), true);
+
+    let file = std::fs::OpenOptions::new()
+        .append(true)
+        .create(false)
+        .open(&path);
+
+    match file {
+        Ok(mut file) => match file.write_all(text.as_bytes()) {
+            Err(why) => sm.log(format!("couldn't append to {}: {}", path, why)),
+            Ok(_) => sm.log(format!("successfully appended to {}", path)),
+        },
+        Err(why) => sm.log(format!("Couldn't Open File: {} : {}", path, why)),
+    }
+}
+
+// ==========================================================
 // STATE
-//==========================================================
+// ==========================================================
 struct State {
     path: String,
     times: u32,
@@ -224,9 +238,7 @@ impl StateManager {
     }
 
     fn log<S: Into<String>>(&self, text: S) {
-        self.state
-            .borrow()
-            .status_area
-            .set_text(text.into().as_str());
+        let text = text.into();
+        self.state.borrow().status_area.set_text(text.as_str());
     }
 }
